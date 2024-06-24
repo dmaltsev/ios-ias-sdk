c38562f79924ceaf8a8b5a9b07c9e3f3e14af529
Denis Maltsev <demaltsev@gmail.com>
2024-06-24 21:54:36 +0300

    Update deployment version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "InAppStorySDK",
     platforms: [
-        .iOS(.v10)
+        .iOS(.v14)
     ],
     products: [
         .library(name: "InAppStorySDK", targets: ["InAppStorySDK"])
